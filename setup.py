051a47e549d5c8c452b1d8b482ec5bfe9bc3849f
Marcus Santos <marcus@marcus-santos.com>
2020-02-26 19:21:45 -0300

    fix submodule missing

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     keywords='spotware openapi2',
     name='spotware_connect',
-    packages=find_packages(include=['spotware_connect']),
+    packages=find_packages(include=['spotware_connect', 'spotware_connect.messages']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
