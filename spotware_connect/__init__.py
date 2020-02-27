44129633fa9168c8794b6ca22c57a9f9466d2165
Marcus Santos <marcus@marcus-santos.com>
2020-02-26 23:48:01 -0300

    Bump version: 0.1.1 → 0.1.2

--- spotware_connect/__init__.py
+++ spotware_connect/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Marcus Santos"""
 __email__ = 'marcus@marcus-santos.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .client import Client  # noqa
 from .protobuf import Protobuf  # noqa
